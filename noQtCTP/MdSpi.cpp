b798daf67424af9c2988b5523c8369032162cc04
65jie <65jiee@gmail.com>
2020-03-27 03:32:00 +0800

    add files

---
+++ noQtCTP/MdSpi.cpp
@@ -0,0 +1,97 @@
+#include "MdSpi.h"
+#include "ThostFtdcUserApiStruct.h"
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+/// 会员代码
+TThostFtdcBrokerIDType g_chBrokerID;
+/// 交易用户代码
+TThostFtdcUserIDType g_chUserID;
+/// 交易用户密码
+TThostFtdcPasswordType g_chPassword;
+/// 交易所代码
+TThostFtdcExchangeIDType g_chExchangeID;
+///合约代码
+TThostFtdcInstrumentIDType	g_chInstrumentID;
+///投资者代码
+TThostFtdcInvestorIDType g_chInvestorID;
+///用户端产品信息
+TThostFtdcProductInfoType	g_chUserProductInfo;
+///认证码
+TThostFtdcAuthCodeType	g_chAuthCode;
+///App代码
+TThostFtdcAppIDType	g_chAppID;
+
+const string BROKER_ID = "8060";
+const string NULL_STR = "";
+
+MdSpi::MdSpi(CThostFtdcMdApi *mdapi){
+	this->mdapi = mdapi;
+	loginRequestID = 10;
+}
+
+void MdSpi::OnFrontConnected(){
+	cout << "已连接上，请求登录" << endl;
+	loginField = new CThostFtdcReqUserLoginField();
+	strcpy(loginField->BrokerID, BROKER_ID.c_str());
+	strcpy(loginField->UserID, NULL_STR.c_str());
+	strcpy(loginField->Password, NULL_STR.c_str());
+	mdapi->ReqUserLogin(loginField, loginRequestID);
+}
+
+void MdSpi::OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo,
+	int nRequestID, bool bIsLast){
+	cout << "登陆成功\n";
+	cout << "交易日：" << mdapi->GetTradingDay() << endl;
+	if (pRspInfo->ErrorID == 0){
+		cout << "请求的登陆成功," << "请求ID为" << loginRequestID << endl;
+		/***************************************************************/
+		cout << "尝试订阅行情" << endl;
+		char *instrumentID[] = { "cu1409" };	//订阅一个合约所以数量为1
+		mdapi->SubscribeMarketData(instrumentID, 1);
+	}
+}
+
+void MdSpi::OnRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, CThostFtdcRspInfoField *pRspInfo,
+	int nRequestID, bool bIsLast){
+
+}
+
+//订阅行情应答
+void MdSpi::OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo,
+	int nRequestID, bool bIsLast){
+	cout << "订阅行情应答" << endl;
+	cout << "合约代码:" << pSpecificInstrument->InstrumentID << endl;
+	cout << "应答信息:" << pRspInfo->ErrorID << " " << pRspInfo->ErrorMsg << endl;
+}
+
+//取消订阅行情应答
+void MdSpi::OnRspUnSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo,
+	int nRequestID, bool bIsLast){
+
+}
+
+void MdSpi::OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData){
+	cout << "===========================================" << endl;
+	cout << "深度行情" << endl;
+	cout << " 交易日:" << pDepthMarketData->TradingDay << endl
+		<< "合约代码:" << pDepthMarketData->InstrumentID << endl
+		<< "最新价:" << pDepthMarketData->LastPrice << endl
+		//<< "上次结算价:" << pDepthMarketData->PreSettlementPrice << endl
+		//<< "昨收盘:" << pDepthMarketData->PreClosePrice << endl
+		//<< "数量:" << pDepthMarketData->Volume << endl
+		//<< "昨持仓量:" << pDepthMarketData->PreOpenInterest << endl
+		<< "最后修改时间" << pDepthMarketData->UpdateTime << endl
+		<< "最后修改毫秒" << pDepthMarketData->UpdateMillisec << endl;
+	//<< "申买价一：" << pDepthMarketData->BidPrice1 << endl
+	//<< "申买量一:" << pDepthMarketData->BidVolume1 << endl
+	//<< "申卖价一:" << pDepthMarketData->AskPrice1 << endl
+	//<< "申卖量一:" << pDepthMarketData->AskVolume1 << endl
+	//<< "今收盘价:" << pDepthMarketData->ClosePrice << endl
+	//<< "当日均价:" << pDepthMarketData->AveragePrice << endl
+	//<< "本次结算价格:" << pDepthMarketData->SettlementPrice << endl
+	//<< "成交金额:" << pDepthMarketData->Turnover << endl
+	//<< "持仓量:" << pDepthMarketData->OpenInterest << endl;
+}
---
+++ noQtCTP/main.cpp
@@ -0,0 +1,51 @@
+#include "ThostFtdcMdApi.h"
+#include "ThostFtdcTraderApi.h"
+#include "MdSpi.h"
+#include "TdSpi.h"
+#include <iostream>
+
+#pragma comment(lib,"thostmduserapi_se.lib")
+#pragma comment(lib,"thosttraderapi_se.lib")
+
+FILE *logfile;
+
+int main(int argc, char *argv[]){
+
+	//CThostFtdcMdApi *mdapi = CThostFtdcMdApi::CreateFtdcMdApi("./md_file/");
+	//MdSpi *mdspi = new MdSpi(mdapi);
+	////注册事件处理对象
+	//mdapi->RegisterSpi(mdspi);
+	////和前置机连接
+	//mdapi->RegisterFront("tcp://mn101.ctp.gtjafutures.com:41205");
+	//std::cout << "1\n";
+	//mdapi->Init();
+	//std::cout << "2\n";
+	//mdapi->Join();
+	//std::cout << "3\n";
+	//mdapi->Release();
+	//std::cout << "4\n";
+	//system("pause");
+
+	auto tdapi = CThostFtdcTraderApi::CreateFtdcTraderApi("./trader_file/");
+	TdSpi *tdspi = new TdSpi(tdapi);
+	//注册事件处理对象
+	tdapi->RegisterSpi(tdspi);
+	//订阅共有流和私有流
+	tdapi->SubscribePublicTopic(THOST_TERT_RESTART);
+	tdapi->SubscribePrivateTopic(THOST_TERT_RESTART);
+	//注册前置机
+	//tdapi->RegisterFront("tcp://180.169.75.19:41205");	//实盘270338
+	tdapi->RegisterFront("tcp://180.168.146.187:10130");	//模拟 交易
+	// tdapi->RegisterFront("tcp://180.168.146.187:10131");	//模拟 行情
+	std::cout << "1\n";
+	//和前置机连接
+	tdapi->Init();
+	std::cout << "2\n";
+	tdapi->Join();
+	std::cout << "3\n";
+	tdapi->Release();
+	std::cout << "4\n";
+	system("pause");
+
+	return 0;
+}
